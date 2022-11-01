6702446ba2fd386fcf9d456436549fd10e91b4ea
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-11-01 16:27:13 -0500

    se agrega el contador de los caracteres del tweet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const counter = document.getElementById("span-counter");
 const convert = document.getElementById("btn-convert");
 
 btnSend.addEventListener('click', sendTweet);
+txtComment.addEventListener('keyup', lettersCounter);
 
 function sendTweet(){
   let userName = txtName.value;
@@ -15,4 +16,15 @@ function sendTweet(){
   paragraphInitial.textContent = txtName.value.trim().charAt(0).toUpperCase();
   paragraphName.textContent = userName;
   resultComment.textContent = userComment;
+}
+
+function lettersCounter(){
+  let total = 280;
+  let remaining = total - txtComment.value.length;
+  if (remaining <= 50) {
+    counter.style.color = "red";
+  }else{
+    counter.style.color = "#0c4876";
+  }
+  counter.textContent = remaining ;
 }
